eb5d548ef61baf46c96a30716bc6187eceffac45
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-04 08:20:46 +0000

    [Update] 入力欄バリデーションの設定

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ApplicationRecord
+
+  validates :body, presence: true, length: { maximum: 100 }
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+  has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body,  presence: true
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
