c1435e4fd2d263c5e66f1c73dee18ddbfa4bc234
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-05 04:41:05 +0000

    [Update] ログイン認証の要求、ログイン/ログアウト後の遷移先を設定

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,5 @@
 class Public::HomesController < ApplicationController
+  skip_before_action :authenticate_user!
 
   def top
   end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,4 +1,5 @@
 class Public::PostsController < ApplicationController
+  skip_before_action :authenticate_user!
   
   def index
   end
