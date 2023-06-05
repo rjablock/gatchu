fa3b85ebe7d378f83bdfcfbaa9e1b383cd02c267
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-05 13:56:02 +0000

    [Update] 新着質問板の配置

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < Public::ApplicationController
 
   def top
     @posts = Post.where(is_active: true).order(id: "DESC").limit(4)
+    @genres = Genre.all
+    @questions = Question.all.order(id: "DESC").limit(3)
   end
 
 end
