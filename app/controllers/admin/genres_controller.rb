f744777a824c4680e7430c839a165e22f7df9e7c
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-08 18:51:46 +0000

    [Update] 作成可能なジャンルの数を最大13項目に設定

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,23 @@
 class Admin::GenresController < Admin::ApplicationController
 
   def create
-    @genre = Genre.new(genre_params)
-    if @genre.save
-      flash[:notice] = "ジャンルの作成に成功しました。"
+    if Genre.count >= 13
+      flash[:alert] = "作成可能なジャンルは最大13項目です。"
       redirect_to request.referer
     else
-      flash[:alert] = "ジャンルの作成に失敗しました。"
-      @genres = Genre.all
-      redirect_to request.referer
+      @genre = Genre.new(genre_params)
+      if @genre.save
+        flash[:notice] = "ジャンルの作成に成功しました。"
+        redirect_to request.referer
+      else
+        flash[:alert] = "ジャンルの作成に失敗しました。"
+        redirect_to request.referer
+      end
     end
   end
 
   def index
-    @genres = Genre.all.page(params[:page]).order(:id).per(10)
+    @genres = Genre.all.order(:id)
   end
 
   def update
