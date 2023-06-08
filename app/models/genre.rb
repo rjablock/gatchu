5546bf9a5301271ec9d5dcef7d9412d57afe310f
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-08 18:58:22 +0000

    [Update] ジャンル名の同名禁止バリデーション設定

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,6 +2,6 @@ class Genre < ApplicationRecord
 
   has_many :posts
 
-  validates :name, presence: true, length: { maximum: 6 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 6 }
 
 end
