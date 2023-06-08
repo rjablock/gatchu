57527ebfc5940f055c807e4fd52cb9f0ed1216c4
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-08 08:18:04 +0000

    [Update] public/today_wodsのshow画面作成

--- app/controllers/public/today_words_controller.rb
+++ app/controllers/public/today_words_controller.rb
@@ -1,9 +1,13 @@
 class Public::TodayWordsController < Public::ApplicationController
-  
+
   def index
+    @today_words = TodayWord.all.page(params[:page]).order(id: "DESC").per(10)
   end
 
   def show
+    @today_word = TodayWord.find(params[:id])
+    @previous_word = TodayWord.where("id < ?", @today_word).order(id: "DESC").first
+    @next_word = TodayWord.where("id > ?", @today_word).order(:id).first
   end
-  
+
 end
