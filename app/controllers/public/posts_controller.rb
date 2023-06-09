62a1a4617c8b6d2bbc69d3a11ff16c8d606da9b0
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-09 13:12:58 +0000

    [Update] ニュース一覧/詳細画面の画面・機能作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,9 +1,22 @@
 class Public::PostsController < Public::ApplicationController
-  
+
   def index
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @posts = Post.where(genre_id: params[:genre_id], is_active: true).page(params[:page]).order(id: "DESC").limit(8)
+    else
+      @posts = Post.where(is_active: true).page(params[:page]).order(id: "DESC").limit(8)
+    end
   end
 
   def show
+    @post = Post.find_by(id: params[:id])
+    @previous_post = Post.where("id < ? AND is_active = ?", @post, true).order(id: "DESC").first
+    @next_post = Post.where("id > ? AND is_active = ?", @post, true).order(:id).first
+
+    if @post.nil? || !@post.is_active
+      redirect_to posts_path, alert: "単語が見つかりません。"
+    end
   end
-  
+
 end
