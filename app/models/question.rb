175f8b1d95f96ec6e897501acb4ecd12fb0df254
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-10 05:43:56 +0000

    [Update] TOPページ上のブックマークボタンの機能作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,14 @@
 class Question < ApplicationRecord
-  
+
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :body,  presence: true, length: { maximum: 300 }
+
+  def bookmarked_by?(user)
+    bookmarks.exists?(user_id: user.id)
+  end
 
 end
