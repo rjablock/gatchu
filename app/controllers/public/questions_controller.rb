514e21405e588bb71ce25ac609a5b00b4ca0cf6a
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-10 11:01:42 +0000

    [Update] ソート機能用の定数記述

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -5,13 +5,16 @@ class Public::QuestionsController < Public::ApplicationController
 
   def create
   end
+  
+  ORDER_BY_POPULAR = 'popular'
+  ORDER_BY_NEW = 'new'
 
   def index
-    if params[:order] == "popular"
-      @order = "popular"
+    if params[:order] == ORDER_BY_POPULAR
+      @order = ORDER_BY_POPULAR
       @questions = Question.all.page(params[:page]).order(views_count: "DESC").per(10)
     else
-      @order = "new"
+      @order = ORDER_BY_NEW
       @questions = Question.all.page(params[:page]).order(id: "DESC").per(10)
     end
   end
