4cdf45beea4e176cd3d66399d54a86f79cb92f32
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-12 18:42:20 +0000

    [Update] admin/answers/indexの画面作成

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -1,6 +1,8 @@
 class Admin::AnswersController < Admin::ApplicationController
 
   def index
+    @question = Question.find(params[:question_id])
+    @answers = @question.answers.page(params[:page]).per(10)
   end
 
   def destroy
