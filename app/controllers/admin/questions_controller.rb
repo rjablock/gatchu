60dd0a1555bf91a2794017cdcc9e02f1a9e1a677
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-12 18:32:47 +0000

    [Update] admin/questions/indexの画面作成

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,12 +1,13 @@
 class Admin::QuestionsController < Admin::ApplicationController
-  
+
   def index
+    @questions = Question.all.page(params[:page]).per(10)
   end
 
   def show
   end
-  
+
   def destroy
   end
-  
+
 end
