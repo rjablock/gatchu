3baff6b83f3133c7d3e2fe79c9d7fb0cbd268138
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-15 19:34:04 +0000

    [Update] public/users/show内に退会リンクの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,9 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, presence: true, length: { maximum: 24 }
+  validates :name, presence: true, length: { maximum: 12 }
   validates :introduction, length: { maximum: 100 }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, length: { maximum: 318 }
 
   enum gender:           { not_answer: 0, male: 1, female: 2 }
   enum age:              { unselected_age: 0, teen: 10, twenties: 20, thirties: 30, forties: 40, fifties: 50, sixties: 60, upper_seventies: 70 }
