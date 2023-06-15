13a9eedd52a5d7347a0c1fa6b1d58b96714f200d
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-15 16:36:02 +0000

    [Update] public/users/showの画面作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,6 +2,8 @@ class Public::UsersController < Public::ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posted_questions = @user.questions
+    @answered_questions = @user.answers.map(&:question).uniq
   end
 
   def edit
