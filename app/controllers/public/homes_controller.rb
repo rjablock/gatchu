70ee1252fba900bc9a61080551ff591004adc6ba
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-16 13:47:33 +0000

    [Update] 有効な「今日の中国語」単語が存在しない場合の画面表示を記述

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < Public::ApplicationController
 
   def top
+    @latest_today_word = TodayWord.where(is_active: true).last
     @posts = Post.where(is_active: true).page(params[:page]).order(id: "DESC").per(4)
     @genres = Genre.all
     @questions = Question.all.order(id: "DESC").limit(3)
