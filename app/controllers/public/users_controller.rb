1d52d33ba0f4b15b1de5459992be8b8c079ac578
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-16 09:05:58 +0000

    [Update] 他ユーザーマイページ編集・退会画面およびその機能へのアクセス制限を追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,35 +2,49 @@ class Public::UsersController < Public::ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posted_questions = @user.questions
-    @answered_questions = @user.answers.map(&:question).uniq
+    if @user.is_deleted
+      redirect_to user_path(current_user), alert: "ユーザーが見つかりません。"
+    else
+      @posted_questions = @user.questions
+      @answered_questions = @user.answers.map(&:question).uniq
 
-    @questions_count_past_month = (0..6).map { |months_ago| @user.posted_questions_count_on(months_ago.months.ago) }
-    @answers_count_past_month = (0..6).map { |months_ago| @user.answered_questions_count_on(months_ago.months.ago) }
+      @questions_count_past_month = (0..6).map { |months_ago| @user.posted_questions_count_on(months_ago.months.ago) }
+      @answers_count_past_month = (0..6).map { |months_ago| @user.answered_questions_count_on(months_ago.months.ago) }
+    end
   end
 
   def edit
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to user_path(@user)
-    else
-      render :edit
+    if @user == current_user
+      if @user.update(user_params)
+        redirect_to user_path(@user)
+      else
+        render :edit
+      end
     end
   end
 
   def confirm
     @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
   end
 
   def quit
-    user = current_user
-    user.update(is_deleted: true)
-    reset_session
-    redirect_to root_path, notice: "退会手続きが完了しました。"
+    user = User.find(params[:id])
+    if user == current_user
+      user.update(is_deleted: true)
+      reset_session
+      redirect_to root_path, notice: "退会手続きが完了しました。"
+    end
   end
 
   private
