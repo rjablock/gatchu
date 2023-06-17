c6c35f9297559dc2b70021ce2424be7d257a062e
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-17 11:50:32 +0000

    [Update] public/questions/index内の人気順を閲覧数→ブックマーク数の順番で参照し降順になるよう変更

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -21,7 +21,7 @@ class Public::QuestionsController < Public::ApplicationController
   def index
     if params[:order] == ORDER_BY_POPULAR
       @order = ORDER_BY_POPULAR
-      @questions = Question.all.page(params[:page]).order(views_count: "DESC").per(10)
+      @questions = Question.all.left_joins(:bookmarks).group('questions.id').page(params[:page]).order('views_count DESC', 'COUNT(bookmarks.id) DESC').per(10)
     else
       @order = ORDER_BY_NEW
       @questions = Question.all.page(params[:page]).order(id: "DESC").per(10)
