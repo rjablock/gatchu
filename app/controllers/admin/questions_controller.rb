f61dd087769fa1d42b975d9dad42ef2ec2f26530
rjablock <129575706+rjablock@users.noreply.github.com>
2023-06-27 16:42:06 +0000

    [Update] Admin側question削除時のフラッシュメッセージ実装

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -8,6 +8,7 @@ class Admin::QuestionsController < Admin::ApplicationController
   def destroy
     @question = Question.find(params[:id])
     @question.destroy
+    flash[:alert] = "質問の削除に成功しました。"
     redirect_to admin_questions_path
   end
 
