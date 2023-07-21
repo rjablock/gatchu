48810a79fb6dda1e99d2be5646ec521c9842eca9
rjablock <129575706+rjablock@users.noreply.github.com>
2023-07-21 09:42:54 +0000

    [Update] admin/questionsビュー内answer関連のJSを共通テンプレートに格納

---
+++ app/javascript/packs/answer.js
@@ -0,0 +1,10 @@
+// 文字数カウンター
+  $(document).ready(function() {
+    const textArea = $("#answer_body");
+    const bodyCount = $("#body-count");
+
+    textArea.on("input", function() {
+      const count = textArea.val().length;
+      bodyCount.text(count + " / 300文字");
+    });
+  });
